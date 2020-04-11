b986452ce777fdda573e51de1580ba45c39761de
Ivan Jukic <jukic@arrival.com>
2020-04-11 13:59:39 +0100

    minor cleanup before transforming into wasm

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-#![allow(dead_code)]
-#![allow(unused_imports)]
-
 /// Using Connect4 module in the main only!
 use crate::connect4::Connect4;
-use crate::models::masks::get_win_masks;
 
 /// List of top level modules!
 mod connect4;
@@ -13,8 +9,6 @@ mod models;
 
 /// Main!
 fn main() {
-    println!("You're playing Connect4!");
-
     // Run game!
     Connect4::run();
 }
